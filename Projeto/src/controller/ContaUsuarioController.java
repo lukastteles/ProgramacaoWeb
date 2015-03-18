257d8f2a1ba588be5cd129db45cec08ed16476f6
LuanaJanaina <luannajnsousa@gmail.com>
2015-03-17 22:06:57 -0300

    Adição de metodos para o teste US01

--- Projeto/src/controller/ContaUsuarioController.java
+++ Projeto/src/controller/ContaUsuarioController.java
@@ -14,6 +14,8 @@ public class ContaUsuarioController {
 			tratamentoCampoLogin(login);
 			tratamentoCampoNome(nome);
 			tratamentoCampoEmail(email);
+			procuraEmailExistente(email);
+			procuraLoginExistente(login);
 			
 			Usuario novoUsuario = new Usuario(login, senha, nome, endereco, email);		 
 			usuarios.add(novoUsuario);
@@ -22,9 +24,52 @@ public class ContaUsuarioController {
 		}
 	}
 	
-
+	
+	private void procuraLoginExistente(String login){
+		for (int i = 0; i < usuarios.size(); i++) {
+			if(usuarios.get(i).getLogin().equals(login)){
+				throw new Error(MensagemErro.LOGIN_EXISTENTE);
+			}	
+		}
+	}
+	
+	private void procuraEmailExistente(String email){
+		for (int i = 0; i < usuarios.size(); i++) {
+			if(usuarios.get(i).getEmail().equals(email)){
+				throw new Error(MensagemErro.EMAIL_EXISTENTE);
+			}	
+		}
+	}
+	
+	private void verificaLoginUsuario(String login, String senha){
+		Usuario usuario = null;
+		
+		for (int i = 0; i < usuarios.size(); i++) {
+			if(usuarios.get(i).getLogin().equals(login)){
+				usuario = usuarios.get(i);
+				if (!usuario.getSenha().equals(senha)) {
+					throw new Error(MensagemErro.LOGIN_INVALIDO);
+				}
+			}	
+		}
+		
+		if (usuario == null) {
+			throw new Error(MensagemErro.USUARIO_INEXISTENTE);
+		}
+		
+	}
+	
 	public int abrirSessao(String login, String senha){		
 		int idSessao = 001;
+		try {
+			//Tratamento dos campos do Usuário
+			tratamentoCampoLogin(login);
+			verificaLoginUsuario(login, senha);
+			
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
 		return idSessao;
 	}
 	
@@ -38,7 +83,10 @@ public class ContaUsuarioController {
 				break;
 			}	
 		}
-		
+				
+		if (usuario == null) {
+			return MensagemErro.USUARIO_INEXISTENTE;
+		}
 		
 		switch(atributo){
 			case "login": return usuario.getLogin();
@@ -46,24 +94,30 @@ public class ContaUsuarioController {
 			case "senha": return usuario.getSenha();
 			case "endereco": return usuario.getEndereco();
 			case "email": return usuario.getEmail();
-			default: return "atributo nao existe";
-			
+			default: return MensagemErro.ATRIBUTO_INEXISTENTE;				
+		}
+		
+	}
+	
+	private void tratamentoEmailExistente(String login, String email) {
+		if (getAtributoUsuario(login, "email").equals(email)){
+			throw new Error(MensagemErro.EMAIL_EXISTENTE);
 		}
 	}
 	
-	private void tratamentoCampoLogin(String valor) throws Exception{
+	private void tratamentoCampoLogin(String valor) {
 		if ( (valor == null) || (valor.equals("")) ) { 
 			throw new Error(MensagemErro.LOGIN_INVALIDO);
 		}		
 	}
 	
-	private void tratamentoCampoNome(String valor) throws Exception{
+	private void tratamentoCampoNome(String valor) {
 		if ( (valor == null) || (valor.equals("")) ) { 
 			throw new Error(MensagemErro.NOME_INVALIDO);
 		}		
 	}
 	
-	private void tratamentoCampoEmail(String valor) throws Exception{
+	private void tratamentoCampoEmail(String valor) {
 		if ( (valor == null) || (valor.equals("")) ) { 
 			throw new Error(MensagemErro.EMAIL_INVALIDO);
 		}		
