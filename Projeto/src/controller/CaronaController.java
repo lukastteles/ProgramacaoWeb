7800ade3ba9d9de34d3c4fafa12cd269b7c39c2f
LuanaJanaina <luannajnsousa@gmail.com>
2015-03-20 09:26:33 -0300

    Ajustes nos metodos para o teste US02
    Adição de novos metodos na classe Carona Controller

--- Projeto/src/controller/CaronaController.java
+++ Projeto/src/controller/CaronaController.java
@@ -1,9 +1,6 @@
 package controller;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
 import model.Carona;
 
--- Projeto/src/view/Teste.java
+++ Projeto/src/view/Teste.java
@@ -5,9 +5,9 @@ import easyaccept.EasyAccept;
 public class Teste {
 
 	public static void main(String[] args) {
-		//args = new String[] {"controller.ContaUsuarioFacade", "testesDeAceitacao/US01.txt"};
-		//args = new String[] {"controller.ContaUsuarioFacade", "testesDeAceitacao/US01.txt", "testesDeAceitacao/US02.txt"};
-		args = new String[] {"controller.ContaUsuarioFacade", "testesDeAceitacao/US02.txt"};
+		//args = new String[] {"controller.Facade", "testesDeAceitacao/US01.txt"};
+		//args = new String[] {"controller.Facade", "testesDeAceitacao/US01.txt", "testesDeAceitacao/US02.txt"};
+		args = new String[] {"controller.Facade", "testesDeAceitacao/US02.txt"};
 		EasyAccept.main(args);
 		
 	}
