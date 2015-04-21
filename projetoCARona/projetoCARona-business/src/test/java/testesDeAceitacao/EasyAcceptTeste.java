36faa40eaf194b3d628a34da8c704d80ebc9b42d
LuanaJanaina <luannajnsousa@gmail.com>
2015-04-21 20:38:04 -0300

    Ajustes nos US's e diagrama de classe
    Fiz apenas ajustes simples e estou enviando o diagrama de classes junto
    do projeto no EA

--- projetoCARona/projetoCARona-business/src/test/java/testesDeAceitacao/EasyAcceptTeste.java
+++ projetoCARona/projetoCARona-business/src/test/java/testesDeAceitacao/EasyAcceptTeste.java
@@ -5,7 +5,14 @@ import easyaccept.EasyAccept;
 public class EasyAcceptTeste {
 	
 	public static void main(String[] args) {
-		args = new String[] {"com.br.uepb.business.FacadeTestBusiness", "src/test/resources/userStories/US07.txt"};
+		
+		args = new String[] {"com.br.uepb.business.FacadeTestBusiness", "src/test/resources/userStories/US01.txt", 
+																		"src/test/resources/userStories/US02.txt",
+																		"src/test/resources/userStories/US03.txt",
+																		"src/test/resources/userStories/US04.txt",
+																		"src/test/resources/userStories/US05.txt",
+																		"src/test/resources/userStories/US06.txt",
+																		"src/test/resources/userStories/US07.txt"};
 		EasyAccept.main(args);
 	}
 }
