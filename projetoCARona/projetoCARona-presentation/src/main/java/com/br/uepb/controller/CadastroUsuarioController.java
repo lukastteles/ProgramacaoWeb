441d1203cbc17d262ca1337612f7a2eb7a6c6daa
LuanaJanaina <luannajnsousa@gmail.com>
2015-06-16 00:40:43 -0300

    Funcionalidade do cadastroUsuarioController OK!
    @LukasTeles já está cadastrando o usuario atraves da view na base de
    dados. Falta os tratamentos de erro dessa classe.
    *Vamos conseguir tudo \0>

--- projetoCARona/projetoCARona-presentation/src/main/java/com/br/uepb/controller/CadastroUsuarioController.java
+++ projetoCARona/projetoCARona-presentation/src/main/java/com/br/uepb/controller/CadastroUsuarioController.java
@@ -15,7 +15,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.br.uepb.business.SessaoBusiness;
 import com.br.uepb.business.UsuarioBusiness;
-import com.br.uepb.domain.UsuarioDomain;
+import com.br.uepb.viewModels.CadastroUsuarioViewModels;
 
 @Controller
 public class CadastroUsuarioController {
@@ -28,16 +28,48 @@ public class CadastroUsuarioController {
 	@Autowired
 	private SessaoBusiness sessaoBusiness;
 	
+	@RequestMapping(value = "/home/cadastroUsuario.html", method = RequestMethod.GET)
+    public ModelAndView showCadastroUsuario(HttpServletRequest request){
+		LOG.debug("Iniciada a execucao do metodo: showCadastroUsuario");
+
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("cadastroUsuario");
+		
+		modelAndView.addObject("usuarioVieModel", new CadastroUsuarioViewModels());
+		
+		LOG.debug("Finalizada a execucao do metodo: showCadastroUsuario");
+		
+		return modelAndView;
+    }
+	
 	@RequestMapping(value = "/home/cadastroUsuario.html", method = RequestMethod.POST)
-	public ModelAndView addNovoUsuario(@ModelAttribute("usuarioDomain") @Valid UsuarioDomain usuarioDomain, BindingResult bindingResult, HttpServletRequest request) throws Exception {
+	public ModelAndView addNovoUsuario(@ModelAttribute("usuarioVieModel") @Valid CadastroUsuarioViewModels usuarioVieModel, BindingResult bindingResult, HttpServletRequest request) throws Exception {
 		
 		LOG.debug("Iniciada a execucao do metodo: addNovoUsuario POST");
 
+
+		System.out.println("login: "+usuarioVieModel.getLogin());
+		System.out.println("senha: "+usuarioVieModel.getSenha());
+		System.out.println("nome: "+usuarioVieModel.getEndereco());
+		
+		
 		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("cadastroUsuario");
+
+		if(bindingResult.hasErrors()){
+			modelAndView.addObject("usuarioVieModel", usuarioVieModel);			
+			return modelAndView;
+		}
 		
-		
+		try {
+			usuarioBusiness.criarUsuario(usuarioVieModel.getLogin(), usuarioVieModel.getSenha(), usuarioVieModel.getNome(), usuarioVieModel.getEndereco(), usuarioVieModel.getEmail());			
+		} catch (Exception e) {
+			LOG.debug("Problemas ao tentar criar um novo usuario no metodo: addNovoUsuario POST - Erro: "+e.getMessage());
+			
+			return modelAndView;
+		}
+
 		modelAndView.setViewName("home");
-		
 		LOG.debug("Finalizada a execucao do metodo: addNovoUsuario POST");
 		
 		return modelAndView;
--- projetoCARona/projetoCARona-presentation/src/main/webapp/WEB-INF/views/home/cadastroUsuario.jsp
+++ projetoCARona/projetoCARona-presentation/src/main/webapp/WEB-INF/views/home/cadastroUsuario.jsp
@@ -1,8 +1,7 @@
 <%@ include file="/WEB-INF/views/includeTags.jsp"%>
 <div class="account-container register">	
-	<div class="content">					
-		<form:form modelAttribute="usuarioDomain" method="post">	
-				
+	<div class="content">							
+		<form:form modelAttribute="usuarioVieModel" method="post">					
 			<div class="form-group">
     			<label>Login: </label>
     			<form:input path="login" type="text" class="form-control" placeholder="Insira o Login" />
@@ -13,21 +12,21 @@
 				<form:input path="senha" type="password" class="form-control" placeholder="Insira a Senha"/>
 				
   			</div>
-  			<div class="form-group">
-    			<label>Nome: </label>
-				<form:input path="perfil.nome" type="text" class="form-control" placeholder="Insira o Nome"/>
-				
-  			</div>
-  			<div class="form-group">
-    			<label>Endereço: </label>
-				<form:input path="perfil.endereco" type="text" class="form-control" placeholder="Insira o Endereço"/>
-				
-  			</div>  
-  			<div class="form-group">
-    			<label>Email: </label>    			
-				<form:input path="perfil.email" type="text" class="form-control" placeholder="Insira o Email"/>
-				
-  			</div>
+  			
+	  		<div class="form-group">
+	    		<label>Nome: </label>
+				<form:input path="nome"  type="text" class="form-control" placeholder="Insira o Nome"/>		
+	  		</div>
+	  		
+	  		<div class="form-group">
+	    		<label>Endereço: </label>
+				<form:input path="endereco" type="text" class="form-control" placeholder="Insira o Endereço"/>		
+	  		</div>  
+	  		
+	  		<div class="form-group">
+	    		<label>Email: </label>    			
+				<form:input path="email" type="text" class="form-control" placeholder="Insira o Email"/>				
+	  		</div>
   			
   			<form:button type="submit" class="btn btn-default">Submit</form:button>			
 		</form:form>
